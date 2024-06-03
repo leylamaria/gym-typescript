e11b0b01c9882fb8228ad1cda515c55bd66c2c97
Leyla <157950987+leylamaria@users.noreply.github.com>
2024-06-03 12:21:34 +0200

    Integrated ContactUs component with React-hook-forms

--- gym-typescript/src/scenes/ourClasses/Class.tsx
+++ gym-typescript/src/scenes/ourClasses/Class.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Class = ({ name, description, image }: Props) => {
   const overlayStyles = `p-5 absolute z-30 flex
   h-[380px] w-[450px] flex-col items-center justify-center 
-  whitespace-normal bg-[#ae8cbc] text-center text-white 
+   whitespace-normal bg-[#ae8cbc] text-center text-white 
   opacity-0 transition duration-500 hover:opacity-90`;
 
   return (
@@ -16,7 +16,7 @@ const Class = ({ name, description, image }: Props) => {
         <p className="text-2xl">{name}</p>
         <p className="mt-5">{description}</p>
       </div>
-      <img alt={`$image`} src={image} />
+      <img alt={`${image}`} src={image} />
     </li>
   );
 };
